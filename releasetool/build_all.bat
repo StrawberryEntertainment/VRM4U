c82652a83806cb9bd98691f62b769c0462437033
ruyo <ruyoruyo@gmail.com>
2019-04-07 21:07:23 +0900

    add 4.22

--- releasetool/build_all.bat
+++ releasetool/build_all.bat
@@ -23,6 +23,12 @@ if not %errorlevel% == 0 (
     goto err
 )
 
+call build_ver.bat 4.22 Win64 MyProjectBuildScriptEditor VRM4U_4_22.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
 call build_ver.bat 4.21 Win64 MyProjectBuildScript VRM4U_4_21_gam.zip
 if not %errorlevel% == 0 (
     echo [ERROR] :P
