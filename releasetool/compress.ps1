8a905f04eb2a615a92ba07f2ea09b27af1e2a1ba
ruyo <ruyoruyo@gmail.com>
2019-05-14 03:37:53 +0900

    update release tool

--- releasetool/compress.ps1
+++ releasetool/compress.ps1
@@ -1,4 +1,5 @@
 
+Remove-Item -Recurse ../Intermediate
 Remove-Item -Recurse ../Plugins/VRM4U/Intermediate
 Remove-Item -Recurse ../Plugins/VRM4U/Binaries/Win64/*.pdb
 Remove-Item -Recurse ../Plugins/VRM4U/Source
