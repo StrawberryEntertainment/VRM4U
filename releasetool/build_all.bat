1e56a9f9ed553651fdeceb717ab50c462351140e
ruyo <ruyoruyo@gmail.com>
2019-07-20 23:09:10 +0900

    update: releaase tool

--- releasetool/build_all.bat
+++ releasetool/build_all.bat
@@ -1,21 +1,15 @@
-
-call build_ver.bat 4.19 Win64 MyProjectBuildScript VRM4U_4_19.zip
+call build_ver.bat 4.23 Win64 MyProjectBuildScript VRM4U_4_23.zip
 if not %errorlevel% == 0 (
     echo [ERROR] :P
     goto err
 )
 
-call build_ver.bat 4.20 Win64 MyProjectBuildScript VRM4U_4_20.zip
+
+call build_ver.bat 4.22 Win64 MyProjectBuildScript VRM4U_4_22.zip
 if not %errorlevel% == 0 (
     echo [ERROR] :P
     goto err
 )
-
-::call build_ver.bat 4.20 Android MyProjectBuildScript VRM4U_4_20_android.zip::
-::if not %errorlevel% == 0 (
-::    echo [ERROR] :P
-::    goto err
-::)
 
 call build_ver.bat 4.21 Win64 MyProjectBuildScript VRM4U_4_21.zip
 if not %errorlevel% == 0 (
@@ -23,11 +17,26 @@ if not %errorlevel% == 0 (
     goto err
 )
 
-call build_ver.bat 4.22 Win64 MyProjectBuildScript VRM4U_4_22.zip
+
+call build_ver.bat 4.20 Win64 MyProjectBuildScript VRM4U_4_20.zip
 if not %errorlevel% == 0 (
     echo [ERROR] :P
     goto err
 )
+
+call build_ver.bat 4.20 Android MyProjectBuildScript VRM4U_4_20_android.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+call build_ver.bat 4.19 Win64 MyProjectBuildScript VRM4U_4_19.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+
 
 call build_ver.bat 4.21 Win64 MyProjectBuildScript VRM4U_4_21_gam.zip
 if not %errorlevel% == 0 (
